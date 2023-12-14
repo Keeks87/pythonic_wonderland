3431a7e36d258b6b95dfbc974d4ab940e350ed4a
Keeks87 <keenandup87@gmail.com>
2023-12-14 21:02:38 +0200

    Update

--- pythonic_wonderland/settings.py
+++ pythonic_wonderland/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-!*7v6n=b7ond!&*6^fp*#kw5hndh4(jf-(y#hca+p@-%+(wr+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
