8c7a58ea01d2ee5eb5c18917810d34b0fb55fd00
Keenan <110526824+Keeks87@users.noreply.github.com>
2024-02-08 16:29:03 +0200

    Update

--- pythonic_wonderland/settings.py
+++ pythonic_wonderland/settings.py
@@ -13,10 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
-from dotenv import load_dotenv
 
-# Load .env file
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+SECRET_KEY = 'django-insecure-!*7v6n=b7ond!&*6^fp*#kw5hndh4(jf-(y#hca+p@-%+(wr+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
